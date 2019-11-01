495d47f44899222d69242ea9babc8a65ab4d2732
metaeni1 <metaeni1@students.zhaw.ch>
2019-11-01 14:03:06 +0100

    Visual Studio Code gelöscht

--- src/main/java/ch/zhaw/gpi/twitterreview/GetUserInformationDelegate.java
+++ src/main/java/ch/zhaw/gpi/twitterreview/GetUserInformationDelegate.java
@@ -1,40 +1,44 @@
 package ch.zhaw.gpi.twitterreview;
 
+import java.util.Optional;
+
 import javax.inject.Named;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
-import org.camunda.bpm.engine.delegate.JavaDelegate;
-import org.camunda.bpm.engine.impl.persistence.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import java.util.Optional;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+//import org.camunda.bpm.engine.impl.persistence.entity.UserEntity;
+
 /**
  * GetUserInformationDelegate
  */
 @Named("getUserInformationAdapter")
-public class GetUserInformationDelegate implements JavaDelegate{
+public class GetUserInformationDelegate implements JavaDelegate {
 
     @Autowired
     private UserRepository userRepository;
 
     @Override
-    public void execute(DelegateExecution execution) throws Exception {
+    public void execute(DelegateExecution execution) throws Exception{
 
         String userName = (String) execution.getVariable("anfrageStellenderBenutzer");
-
         String fullName;
 
-        Optional <UserEntity> user = userRepository.findById(userName);
+
+        Optional<ch.zhaw.gpi.twitterreview.UserEntity> user = userRepository.findById(userName);
 
         if(user.isPresent()){
             fullName = user.get().getFirstName() + " " + user.get().getLastName();
+
         }
-        else {
+        else{
             fullName = "Mr. X";
         }
 
         execution.setVariable("vollerName", fullName);
         
-    }
 
+    }
     
 }
+
